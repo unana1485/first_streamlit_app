15e1d5c2196b9b5fd5b4460ab5f79dfae374a0f8
Ujwala Nanavare <ujwala.snowflake.training@gmail.com>
2022-09-23 15:32:29 -0500

    Update streamlit_app.py
    added more menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit
 streamlit.title("New Healthy Diner")
 
-streamlit.subtitle("Breakfast Menu")
+streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 and Bluberry Oatmeal")
-
+streamlit.text("Banana Pancakes")
+streamlit.text("Hashbrown pancakes")
