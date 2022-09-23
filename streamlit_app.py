26f012610dd6ec10c244ea611028e5697da2ce0e
Ujwala Nanavare <ujwala.snowflake.training@gmail.com>
2022-09-23 15:30:33 -0500

    Update streamlit_app.py
    corrected typo in streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@
 import streamlit
 streamlit.title("New Healthy Diner")
 
-strealit.subtitle("Breakfast Menu")
-stremlit.text("Omega 3 and Bluberry Oatmeal")
+streamlit.subtitle("Breakfast Menu")
+streamlit.text("Omega 3 and Bluberry Oatmeal")
 
