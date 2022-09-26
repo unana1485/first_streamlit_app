3146a3f7d703f808aab950a904293e00393d6954
Ujwala Nanavare <ujwala.snowflake.training@gmail.com>
2022-09-26 10:31:20 -0500

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,5 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # disply the table on the page
-steamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
 
