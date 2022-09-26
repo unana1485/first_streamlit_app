419c1edb68b7dc50a1ffe54092d3af90a0e90808
Ujwala Nanavare <ujwala.snowflake.training@gmail.com>
2022-09-26 10:13:17 -0500

    Update streamlit_app.py
    reading CSV from s3 bucket using pandas,

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 
 import streamlit
+import pandas as pd
+
 streamlit.title("New Healthy Diner")
 
 streamlit.header("🍲Breakfast Favorites")
@@ -9,3 +11,5 @@ streamlit.text("🥚 Hashbrown pancakes")
 streamlit.text("🥑 Avacado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
